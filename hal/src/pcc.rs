8332262e9ac72fd423d3054d8e4c261169c40ebe
Curious <Curious@curious.host>
2026-06-12 03:32:57 +0800

    fix(pcc,scg): correct PCS clock source values and add SCG divider initialization
    PCC: add pcs_3() (FIRC) for all PCS-capable peripherals (LPSPI, LPI2C,
    LPIT, LPUART, TPM, PCC1 variants). PORT, CAU3, TRNG remain CGC-only.
    
    SCG: add FIRCDIV1/2/3, FIRCCFG range, SIRC/SOSC/LPFLL async dividers.
    Extend Config struct with bus_div/ext_div. Fix slow_hz() to include
    core_div factor. Make clock enables conditional on !vld so boot ROM
    state is not disturbed. Fix LPFLLDIV writer chain (lpflldiv1_1).
    Add firc_div2_hz() and firc_div3_hz() public accessors.

--- hal/src/pcc.rs
+++ hal/src/pcc.rs
@@ -12,40 +12,40 @@ pub fn enable_port_clock(pcc: &pac::Pcc0, port: u8) {
 
 pub fn enable_lpspi_clock(pcc: &pac::Pcc0, instance: u8) {
     match instance {
-        0 => { pcc.pcc_lpspi0().write(|w| w.cgc().cgc_1()); }
-        1 => { pcc.pcc_lpspi1().write(|w| w.cgc().cgc_1()); }
-        2 => { pcc.pcc_lpspi2().write(|w| w.cgc().cgc_1()); }
+        0 => { pcc.pcc_lpspi0().write(|w| w.pcs().pcs_3().cgc().cgc_1()); }
+        1 => { pcc.pcc_lpspi1().write(|w| w.pcs().pcs_3().cgc().cgc_1()); }
+        2 => { pcc.pcc_lpspi2().write(|w| w.pcs().pcs_3().cgc().cgc_1()); }
         _ => {}
     }
 }
 
 pub fn enable_lpi2c_clock(pcc: &pac::Pcc0, instance: u8) {
     match instance {
-        0 => { pcc.pcc_lpi2c0().write(|w| w.cgc().cgc_1()); }
-        1 => { pcc.pcc_lpi2c1().write(|w| w.cgc().cgc_1()); }
-        2 => { pcc.pcc_lpi2c2().write(|w| w.cgc().cgc_1()); }
+        0 => { pcc.pcc_lpi2c0().write(|w| w.pcs().pcs_3().cgc().cgc_1()); }
+        1 => { pcc.pcc_lpi2c1().write(|w| w.pcs().pcs_3().cgc().cgc_1()); }
+        2 => { pcc.pcc_lpi2c2().write(|w| w.pcs().pcs_3().cgc().cgc_1()); }
         _ => {}
     }
 }
 
 pub fn enable_lpit0_clock(pcc: &pac::Pcc0) {
-    pcc.pcc_lpit0().write(|w| w.cgc().cgc_1());
+    pcc.pcc_lpit0().write(|w| w.pcs().pcs_3().cgc().cgc_1());
 }
 
 pub fn enable_lpuart_clock(pcc: &pac::Pcc0, instance: u8) {
     match instance {
-        0 => { pcc.pcc_lpuart0().write(|w| w.cgc().cgc_1()); }
-        1 => { pcc.pcc_lpuart1().write(|w| w.cgc().cgc_1()); }
-        2 => { pcc.pcc_lpuart2().write(|w| w.cgc().cgc_1()); }
+        0 => { pcc.pcc_lpuart0().write(|w| w.pcs().pcs_3().cgc().cgc_1()); }
+        1 => { pcc.pcc_lpuart1().write(|w| w.pcs().pcs_3().cgc().cgc_1()); }
+        2 => { pcc.pcc_lpuart2().write(|w| w.pcs().pcs_3().cgc().cgc_1()); }
         _ => {}
     }
 }
 
 pub fn enable_tpm_clock(pcc: &pac::Pcc0, instance: u8) {
     match instance {
-        0 => { pcc.pcc_tpm0().write(|w| w.cgc().cgc_1()); }
-        1 => { pcc.pcc_tpm1().write(|w| w.cgc().cgc_1()); }
-        2 => { pcc.pcc_tpm2().write(|w| w.cgc().cgc_1()); }
+        0 => { pcc.pcc_tpm0().write(|w| w.pcs().pcs_3().cgc().cgc_1()); }
+        1 => { pcc.pcc_tpm1().write(|w| w.pcs().pcs_3().cgc().cgc_1()); }
+        2 => { pcc.pcc_tpm2().write(|w| w.pcs().pcs_3().cgc().cgc_1()); }
         _ => {}
     }
 }
@@ -63,21 +63,49 @@ pub fn enable_trng_clock(pcc: &pac::Pcc1) {
 }
 
 pub fn enable_lpit1_clock(pcc: &pac::Pcc1) {
-    pcc.pcc_lpit1().write(|w| w.cgc().cgc_1());
+    pcc.pcc_lpit1().write(|w| w.pcs().pcs_3().cgc().cgc_1());
 }
 
 pub fn enable_tpm3_clock(pcc: &pac::Pcc1) {
-    pcc.pcc_tpm3().write(|w| w.cgc().cgc_1());
+    pcc.pcc_tpm3().write(|w| w.pcs().pcs_3().cgc().cgc_1());
 }
 
 pub fn enable_lpi2c3_clock(pcc: &pac::Pcc1) {
-    pcc.pcc_lpi2c3().write(|w| w.cgc().cgc_1());
+    pcc.pcc_lpi2c3().write(|w| w.pcs().pcs_3().cgc().cgc_1());
 }
 
 pub fn enable_lpspi3_clock(pcc: &pac::Pcc1) {
-    pcc.pcc_lpspi3().write(|w| w.cgc().cgc_1());
+    pcc.pcc_lpspi3().write(|w| w.pcs().pcs_3().cgc().cgc_1());
 }
 
 pub fn enable_lpuart3_clock(pcc: &pac::Pcc1) {
-    pcc.pcc_lpuart3().write(|w| w.cgc().cgc_1());
+    pcc.pcc_lpuart3().write(|w| w.pcs().pcs_3().cgc().cgc_1());
+}
+
+pub fn enable_adc0_clock(pcc: &pac::Pcc0) {
+    pcc.pcc_adc0().write(|w| w.pcs().pcs_3().cgc().cgc_1());
+}
+
+pub fn enable_crc0_clock(pcc: &pac::Pcc0) {
+    pcc.pcc_crc0().write(|w| w.cgc().cgc_1());
+}
+
+pub fn enable_sema42_0_clock(pcc: &pac::Pcc0) {
+    pcc.pcc_sema42_0().write(|w| w.cgc().cgc_1());
+}
+
+pub fn enable_sema42_1_clock(pcc: &pac::Pcc1) {
+    pcc.pcc_sema42_1().write(|w| w.cgc().cgc_1());
+}
+
+pub fn enable_ewm_clock(pcc: &pac::Pcc0) {
+    pcc.pcc_ewm().write(|w| w.cgc().cgc_1());
+}
+
+pub fn enable_mua_clock(pcc: &pac::Pcc0) {
+    pcc.pcc_mua().write(|w| w.cgc().cgc_1());
+}
+
+pub fn enable_lpdac0_clock(pcc: &pac::Pcc0) {
+    pcc.pcc_lpdac0().write(|w| w.cgc().cgc_1());
 }
--- hal/src/scg.rs
+++ hal/src/scg.rs
@@ -18,15 +18,29 @@ pub struct Config {
     pub source: ClockSource,
     pub core_div: u8,
     pub slow_div: u8,
+    pub bus_div: u8,
+    pub ext_div: u8,
 }
 
 impl Config {
     pub const fn firc_48mhz() -> Self {
-        Self { source: ClockSource::Firc, core_div: 0, slow_div: 1 }
+        Self {
+            source: ClockSource::Firc,
+            core_div: 0,
+            slow_div: 1,
+            bus_div: 0,
+            ext_div: 0,
+        }
     }
 
     pub const fn lpfll_72mhz() -> Self {
-        Self { source: ClockSource::Lpfll, core_div: 0, slow_div: 1 }
+        Self {
+            source: ClockSource::Lpfll,
+            core_div: 0,
+            slow_div: 1,
+            bus_div: 0,
+            ext_div: 0,
+        }
     }
 
     pub const fn core_hz(&self) -> u32 {
@@ -34,7 +48,9 @@ impl Config {
     }
 
     pub const fn slow_hz(&self) -> u32 {
-        source_hz(self.source) / (self.slow_div as u32 + 1)
+        let core_div = self.core_div as u32;
+        let slow_div = self.slow_div as u32;
+        source_hz(self.source) / (core_div + 1) / (slow_div + 1)
     }
 }
 
@@ -60,22 +76,49 @@ impl Scg {
     pub fn configure(&self, config: &Config) {
         match config.source {
             ClockSource::Firc => {
-                self.regs.firccsr().write(|w| w.fircen().fircen_1());
-                while self.regs.firccsr().read().fircerr().is_fircerr_1() {}
-                while !self.regs.firccsr().read().fircvld().bit() {}
+                self.regs.fircdiv().write(|w| unsafe {
+                    w.fircdiv1().bits(1)
+                        .fircdiv2().bits(1)
+                        .fircdiv3().bits(1)
+                });
+                self.regs.firccfg().write(|w| w.range().range_0());
+                if !self.regs.firccsr().read().fircvld().bit() {
+                    self.regs.firccsr().write(|w| w.fircen().fircen_1());
+                    while !self.regs.firccsr().read().fircvld().bit() {}
+                }
             }
             ClockSource::Sirc => {
-                self.regs.sirccsr().write(|w| w.sircen().sircen_1());
-                while !self.regs.sirccsr().read().sircvld().bit() {}
+                self.regs.sircdiv().write(|w| unsafe {
+                    w.sircdiv1().bits(1)
+                        .sircdiv2().bits(1)
+                        .sircdiv3().bits(1)
+                });
+                if !self.regs.sirccsr().read().sircvld().bit() {
+                    self.regs.sirccsr().write(|w| w.sircen().sircen_1());
+                    while !self.regs.sirccsr().read().sircvld().bit() {}
+                }
             }
             ClockSource::Sosc => {
-                self.regs.sosccsr().write(|w| w.soscen().soscen_1());
-                while !self.regs.sosccsr().read().soscvld().bit() {}
+                self.regs.soscdiv().write(|w| unsafe {
+                    w.soscdiv1().bits(1)
+                        .soscdiv2().bits(1)
+                        .soscdiv3().bits(1)
+                });
+                if !self.regs.sosccsr().read().soscvld().bit() {
+                    self.regs.sosccsr().write(|w| w.soscen().soscen_1());
+                    while !self.regs.sosccsr().read().soscvld().bit() {}
+                }
             }
             ClockSource::Lpfll => {
-                self.regs.lpfllcsr().write(|w| w.lpfllen().lpfllen_1());
+                self.regs.lpflldiv().write(|w| {
+                    w.lpflldiv1().lpflldiv1_1()
+                        .lpflldiv2().lpflldiv2_1()
+                });
                 self.regs.lpfllcfg().write(|w| w.fsel().fsel_1());
-                while !self.regs.lpfllcsr().read().lpfllvld().bit() {}
+                if !self.regs.lpfllcsr().read().lpfllvld().bit() {
+                    self.regs.lpfllcsr().write(|w| w.lpfllen().lpfllen_1());
+                    while !self.regs.lpfllcsr().read().lpfllvld().bit() {}
+                }
             }
         }
 
@@ -121,6 +164,18 @@ pub fn configure(_scg: &pac::Scg, config: &Config) {
 
 pub fn clock_hz() -> u32 {
     Scg::new().clock_hz()
+}
+
+pub fn firc_div2_hz() -> u32 {
+    let scg = unsafe { &*(pac::Scg::ptr() as *const pac::scg::RegisterBlock) };
+    let div = scg.fircdiv().read().fircdiv2().bits();
+    if div == 0 { 0 } else { FIRC_HZ / div as u32 }
+}
+
+pub fn firc_div3_hz() -> u32 {
+    let scg = unsafe { &*(pac::Scg::ptr() as *const pac::scg::RegisterBlock) };
+    let div = scg.fircdiv().read().fircdiv3().bits();
+    if div == 0 { 0 } else { FIRC_HZ / div as u32 }
 }
 
 pub fn slow_hz() -> u32 {
